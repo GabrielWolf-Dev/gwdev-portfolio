3a41ca9946799e860b545caa9b49a57ec715f5f1
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-02-18 18:20:47 -0300

    Finalizando as Meta Tags SEO

--- pages/_app.js
+++ pages/_app.js
@@ -12,13 +12,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Saiba mais sobre minha carreira profissional, tecnologias, interesses futuros, próximos passos e adquira conhecimento nas experiências compartilhadas no Blog do site!" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="" />
+        <meta property="og:url" content="https://gwdev-portfolio.vercel.app/" />
         <meta property="og:title" content="GabrielWolf-Dev Portfólio" />
         <meta property="og:description" content="Saiba mais sobre minha carreira profissional, tecnologias, interesses futuros, próximos passos e adquira conhecimento nas experiências compartilhadas no Blog do site!" />
         <meta property="og:image" content="https://raw.githubusercontent.com/GabrielWolf-Dev/gwdev-portfolio/main/public/imgs/seo-img.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="" />
+        <meta property="twitter:url" content="https://gwdev-portfolio.vercel.app/" />
         <meta property="twitter:title" content="GabrielWolf-Dev Portfólio" />
         <meta property="twitter:description" content="Saiba mais sobre minha carreira profissional, tecnologias, interesses futuros, próximos passos e adquira conhecimento nas experiências compartilhadas no Blog do site!" />
         <meta property="twitter:image" content="https://raw.githubusercontent.com/GabrielWolf-Dev/gwdev-portfolio/main/public/imgs/seo-img.png" />
